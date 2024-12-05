f87804c3d649b6bd96798947b68712bbccfa35bf
Muhammad Rakin <68525714+Fauxtarantula@users.noreply.github.com>
2024-12-05 20:05:24 +0800

    Update Sense_can.h

--- Sense_can/src/Sense_can.h
+++ Sense_can/src/Sense_can.h
@@ -8,8 +8,9 @@
 
 class Sense_can{
   public : void Setup_can(int baudrate, int bitrate);
-  public : void IMU_low(float data[2], int addr, uint32_t timebuff);
-  public : void IMU_hi_depth(float data[2], int addr, uint32_t timebuff);
-
+  public : void IMU_low(float data[2], int addr = 0x13, uint32_t timebuff = 2000);
+  public : void IMU_hi_depth(float data[2], int addr = 0x14, uint32_t timebuff = 2000);
+  public : void PHT(float data[2], int addr = 0x16, uint32_t timebuff = 2000);
+  public : void Ultra(float data, int addr = 0x17, uint32_t timebuff = 2000);
   public : void Heartbeat(); //done in setup to check ping with jetson
 };
