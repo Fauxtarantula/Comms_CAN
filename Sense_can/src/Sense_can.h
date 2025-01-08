bf56f30edecba096431047594278c26d9c76a040
Muhammad Rakin <68525714+Fauxtarantula@users.noreply.github.com>
2025-01-08 16:41:55 +0800

    Update Sense_can.h
    got rid of redundant names

--- Sense_can/src/Sense_can.h
+++ Sense_can/src/Sense_can.h
@@ -1,16 +1,9 @@
-#pragma once
-
-#ifndef __IMXRT1062__
-  #error "Check whether teensy"
-#endif
-
 #include "ACAN_T4.h"
+#include "ACAN_T4_Settings.h"
 
 class Sense_can{
-  public : void Setup_can(int baudrate, int bitrate);
-  public : void IMU_low(float data[2], int addr = 0x13, uint32_t timebuff = 2000);
-  public : void IMU_hi_depth(float data[2], int addr = 0x14, uint32_t timebuff = 2000);
-  public : void PHT(float data[2], int addr = 0x16, uint32_t timebuff = 2000);
-  public : void Ultra(float data, int addr = 0x17, uint32_t timebuff = 2000);
+  public : void Setup_can(int bitrate);
+  public : void send2data(float data[2], int addr = 0x13, uint32_t timebuff = 0);
+  public : void send1data(float data, int addr = 0x17, uint32_t timebuff = 0);
   public : void Heartbeat(); //done in setup to check ping with jetson
 };
